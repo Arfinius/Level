8330ed690720647fbaa71f8166b9e45692cae251
ArturKrolikowski <artur.krolikowski@vp.pl>
2024-05-18 21:15:51 +0200

    Update RestartScript.cs

--- Assets/RestartScript.cs
+++ Assets/RestartScript.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RestartScript : MonoBehaviour
 {
+    string name;
     // Start is called before the first frame update
     void Start()
     {
-        
+        name = "MainScene";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown("r"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 }
